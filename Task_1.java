ae9049a3e8f5a8fa8cd49b571a73b8e53322dffe
Таня <tanadakonova12@gmail.com>
2024-05-18 13:59:54 +0300

    Ver.1

---
+++ Task_1.java
@@ -0,0 +1,12 @@
+public class Task_1 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
