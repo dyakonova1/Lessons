da71b3ba786ede14f90597abd7d938952b988339
Таня <tanadakonova12@gmail.com>
2024-05-20 15:55:27 +0300

    Ver.8

---
+++ Task_8.java
@@ -0,0 +1,9 @@
+public class Task_8 {
+    public static void main(String[] args) {
+        printLine("Hello", 3);
+    }
+    static void printLine(String line, int n) {
+        for (int i = 0; i < n; i++)
+            System.out.println(line);}
+
+}
