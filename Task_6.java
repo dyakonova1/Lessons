9056318e5186d53f29a730ecdd2aa6a9eb930f51
Таня <tanadakonova12@gmail.com>
2024-05-26 20:20:19 +0300

    Ver.14

--- Task_6.java
+++
@@ -1,9 +0,0 @@
-public class Task_6 {
-    public static void main(String[] args) {
-        positiveOrNegative(-1);
-    }
-
-    static void positiveOrNegative(int a) {
-        System.out.println(a >= 0 ? "Положительное" : "Отрицательное");
-    }
-}
--- Task_8.java
+++
@@ -1,8 +0,0 @@
-public class Task_8 {
-    public static void main(String[] args) {
-        printLine("Hello", 3);
-    }
-    static void printLine(String line, int n) {
-        for (int i = 0; i < n; i++)
-            System.out.println(line);}
-}
