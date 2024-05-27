9d7ec22d314eb91bf70996ac099f25a898244b0a
Таня <tanadakonova12@gmail.com>
2024-05-27 23:28:06 +0300

    Ver.14

---
+++ Lesson_3/Park.java
@@ -0,0 +1,15 @@
+package Lesson_3;
+
+public class Park {
+    public static class Attractions {
+        public String name;
+        public String time;
+        public double cost;
+
+    public Attractions(String name, String time, double cost){
+        this.name = name;
+        this.time = time;
+        this.cost = cost;
+    }
+    }}
+
